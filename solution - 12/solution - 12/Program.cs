c1ef22f0c1cf4b9744a208e3d40385b8f71473ba
Nurullah Coşkun <168783018+AsNott2000@users.noreply.github.com>
2024-05-17 18:17:01 +0300

    Add files via upload

---
+++ solution - 12/solution - 12/Program.cs
@@ -0,0 +1,33 @@
+﻿
+namespace solution___12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            int triangleNum = 0;
+            int divisor = 0;
+
+            while (divisor <= 500)
+            {
+                triangleNum += i;
+                i++;
+
+                divisor = 0;
+
+                for (int k = 1; k < Math.Sqrt(triangleNum); k++)
+                {
+                    if (triangleNum % k == 0)
+                    {
+                        divisor++;
+                    }
+                }
+                divisor *= 2;
+            }
+
+            Console.WriteLine(triangleNum);
+
+        }
+    }
+}
