f54c807c755e0a1792038b967560553c4635059c
Nurullah Coşkun <168783018+AsNott2000@users.noreply.github.com>
2024-05-17 18:00:58 +0300

    Add files via upload

---
+++ solution - 3/solution - 3/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace solution___3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //What is the largest prime factor of the number 600851475143
+
+
+            long x = 600851475143;
+            int i = 1;
+            int bolen = 2;
+
+            while (x >= bolen)
+            {
+                if (x % bolen == 0)
+                {
+                    x /= bolen;
+                    Console.WriteLine(i + ". element :" + bolen);
+                    i += 1;
+                }
+                else
+                {
+                    bolen += 1;
+                }
+            }
+        }
+    }
+}
