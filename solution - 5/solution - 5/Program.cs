4222976ef23d3afc4cb913675f8d08a05d0ff4e4
Nurullah Coşkun <168783018+AsNott2000@users.noreply.github.com>
2024-05-17 18:13:32 +0300

    Add files via upload

---
+++ solution - 5/solution - 5/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace solution___5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20 ?
+
+            int num = 100;
+            
+            while (true)
+            {
+                int count = 0;
+                num++;
+                for (int i = 2; i < 21; i++)
+                {
+                    count++;
+                    if (num % i != 0)
+                    {
+                        break;  
+                    }
+                    
+                }
+                
+
+                if (count == 19)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine(num);
+        }
+    }
+}
