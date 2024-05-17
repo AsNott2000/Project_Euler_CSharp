10a20810d363c01d9ae6f9f5204ae731c7bca89d
Nurullah Coşkun <168783018+AsNott2000@users.noreply.github.com>
2024-05-17 18:15:53 +0300

    Add files via upload

---
+++ solution - 7/solution - 7/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace solution___7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // What is the 10 001st prime number?
+
+            int sayac = 0;
+            int son_basamak = 0;
+            for (int num = 2; num <= 9999999; num++)
+            {
+                int kontrol = 0;
+
+                for (int i = 2; i < num; i++)
+                {
+                    if (num % i == 0) 
+                    {
+                        kontrol = 1;
+                        break;
+                    }
+                }
+                if (kontrol != 1)
+                {
+                    sayac++;
+                    son_basamak++;
+                    if (son_basamak == 10001)
+                    {
+                        Console.WriteLine("10 001st prime number: {0}", num);
+                        break;
+                    }
+                }
+                
+            }
+            
+
+        }
+    }
+}
