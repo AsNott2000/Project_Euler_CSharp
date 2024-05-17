0b647b14de2537db34664f78ab221f6887f6a468
Nurullah Coşkun <168783018+AsNott2000@users.noreply.github.com>
2024-05-17 18:14:44 +0300

    Add files via upload

---
+++ solution - 6/solution - 6/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace solution___6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+            int first_product;
+            int first_sum = 0;
+            for (int i = 1; i < 101; i++)
+            {
+                first_product = i * i;
+                first_sum += first_product;
+            }
+            int second_sum = 0;
+            int second_product;
+            for (int k = 0; k < 101; k++)
+            {
+                second_sum += k;
+                
+            }
+            second_product = second_sum * second_sum;
+
+            int last = second_product - first_sum;
+
+            Console.WriteLine(last);
+        }
+    }
+}
