e6edf78700d67f6624cb23485d0ee81ccd30e779
Nurullah Coşkun <168783018+AsNott2000@users.noreply.github.com>
2024-05-17 17:59:25 +0300

    Add files via upload

---
+++ solution - 2/solutin - 2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace solution___1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+
+            int fibonacci = 0;
+            int i = 1;
+            int a = 1;
+            int result = 0;
+            while (true)
+            {
+                if (fibonacci < 4000000)
+                {
+                    if (a % 2 == 0)
+                    {
+                        result += a;
+                    }
+                    
+                    fibonacci = i + a;
+                    i = a;
+                    a = fibonacci;
+
+
+                }
+                else
+                {
+                    Console.WriteLine(result);
+                    break;
+                }
+
+
+            }
+
+
+        }
+    }
+}
