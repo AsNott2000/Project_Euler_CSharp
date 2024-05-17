209d5183e493ee7100212a5402ce8506c1498d45
Nurullah Coşkun <168783018+AsNott2000@users.noreply.github.com>
2024-05-17 18:17:58 +0300

    Add files via upload

---
+++ Solution - 14/Solution - 14/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Solution___14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            int chain= 0;
+
+            for (int number = 1000000; number > 0; number--)
+            {
+                int count = 0;
+                int a = number;
+                while (a > 1)
+                {
+                    if (a % 2 == 0)
+                    {
+                        a = a / 2;
+                        count++;
+                    }
+                    else
+                    {
+                        a = (a * 3) + 1;
+                        count++;
+                    }
+                }
+
+                if (count > chain)
+                {
+                    chain = count;
+                    Console.WriteLine(number);
+                    
+                }
+                
+            }
+        }
+    }
+}
